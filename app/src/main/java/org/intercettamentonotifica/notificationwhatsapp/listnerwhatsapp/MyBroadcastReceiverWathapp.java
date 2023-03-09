779f8a0ce73c4bf79b478ee19e84cd8dcda90959
ciromelody <ciromelody@gmail.com>
2023-03-09 16:22:13 +0100

    Intercept listener whatsapp

---
+++ app/src/main/java/org/intercettamentonotifica/notificationwhatsapp/listnerwhatsapp/MyBroadcastReceiverWathapp.java
@@ -0,0 +1,31 @@
+package org.intercettamentonotifica.notificationwhatsapp.listnerwhatsapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.intercettamentonotifica.notificationwhatsapp.R;
+
+
+public class MyBroadcastReceiverWathapp extends BroadcastReceiver {
+MediaPlayer mp;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        mp= MediaPlayer.create(context, R.raw.tiromancino  );
+        mp.start();
+
+
+        Log.v(this.toString(), "Inside onReceive.");
+        Intent startActivityIntent = new Intent(context, SuonaActivity.class);
+        startActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        Log.v(this.toString(), "Starting new activity.");
+        context.getApplicationContext().startActivity(startActivityIntent);
+        Log.v(this.toString(), "Activity should have started.");
+        Toast.makeText(context, "Alarm...di pending intent.", Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/org/intercettamentonotifica/notificationwhatsapp/listnerwhatsapp/MyNotificationListenerWhatsApp.java
@@ -0,0 +1,139 @@
+package org.intercettamentonotifica.notificationwhatsapp.listnerwhatsapp;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import org.intercettamentonotifica.notificationwhatsapp.MainActivity;
+import org.intercettamentonotifica.notificationwhatsapp.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MyNotificationListenerWhatsApp extends NotificationListenerService  {
+
+    Context context ;
+    //static MyListener myListener ;
+    String titleData = "", textData = "" ;
+
+
+    @Override
+    public void onCreate() {
+        super .onCreate() ;
+        context = getApplicationContext() ;
+        Log.d("WAT","nOME DA INTERCETTARE:"+getNomeDaIntercettare());
+
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+
+        if (sbn.getPackageName().equals("com.whatsapp"))
+        // if (!sbn.getPackageName().equals(""))
+                 {
+            String pack = sbn.getPackageName();
+            Bundle extras = sbn.getNotification().extras;
+            if (extras.getString("android.title") != null) {
+                  titleData = extras.getString("android.title");
+            } else {
+                 titleData = "No Title";
+            }
+            if (extras.getCharSequence("android.text") != null) {
+                  textData = extras.getCharSequence("android.text").toString();
+            } else {
+               textData = "No data";
+            }
+
+
+           if (!(pack.equals("android"))){
+               // myListener.setValue(sbn.getPackageName(), titleData, textData, String.valueOf(sbn.getTag()));
+                //registra sul database ma a noi interessa solo per debug quindi lo commentiamo
+                Log.d("WAT","MESSAGGIO DA WATSAPP:"+sbn.getPackageName()+":"+titleData+":"+String.valueOf(sbn.getTag()));
+               if( titleData.contains( getNomeDaIntercettare())){
+                   impostaOrario();
+                 // lanciaactivity();// non funziona
+                   Log.d("WAT","INTERCETTATO:"+sbn.getPackageName()+":"+titleData+":"+String.valueOf(sbn.getTag()));
+                   //imposta allarme
+               }
+
+           }
+
+        }
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+
+    }
+
+    @Override
+    public void onNotificationRankingUpdate(RankingMap rankingMap) {
+        super.onNotificationRankingUpdate(rankingMap);
+    }
+
+   /* public void setListener (MyListener myListener) {
+        MyNotificationListenerWhatsApp. myListener = myListener ;
+    }*/
+
+
+    private void setAlarm(Calendar targetCal){
+       // info.setText("\n\n***\n" + "Alarm is set@ " + targetCal.getTime() + "\n" + "***\n");
+        Ringtone ringTone;
+        int RQS_1 = 1;
+        Intent intent = new Intent(getBaseContext(), MyBroadcastReceiverWathapp.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), RQS_1, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
+        AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(), pendingIntent);
+        Uri uriAlarm = RingtoneManager.getDefaultUri(R.raw.tiromancino);
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        ringTone = RingtoneManager.getRingtone(getApplicationContext(), uriAlarm);
+        /*if((targetCal.getTime()).equals(currentDateTimeString)){
+            ringTone = RingtoneManager.getRingtone(getApplicationContext(), uriAlarm);
+        }*/
+    }
+    private void impostaOrario(){
+        Calendar current = Calendar.getInstance();
+        Calendar cal = Calendar.getInstance();
+        Calendar now = Calendar.getInstance();
+        cal.set(now.get(Calendar.YEAR),
+                now.get(Calendar.MONTH),
+                now.get(Calendar.DAY_OF_MONTH),
+                now.get(Calendar.HOUR_OF_DAY),
+                now.get(Calendar.MINUTE),
+                10);
+        setAlarm(cal);
+
+    }
+    String getNomeDaIntercettare(){
+        SharedPreferences sharedPref = getSharedPreferences("NOTIFICHE",Context.MODE_PRIVATE);
+       String defaultValue = getResources().getString(R.string.default_key);
+       String nome = sharedPref.getString(getString(R.string.saved_nome_whatsapp_key), defaultValue);
+       return nome.trim();
+    }
+void lanciaactivity(){
+    Intent notifyIntent = new Intent(this, SuonaActivity.class);
+// Set the Activity to start in a new, empty task
+   /* notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+            | Intent.FLAG_ACTIVITY_CLEAR_TASK);*/
+// Create the PendingIntent
+    PendingIntent notifyPendingIntent = PendingIntent.getActivity(
+            this, 2, notifyIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE
+    );
+    AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+    alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+5000, notifyPendingIntent);
+}
+
+}
